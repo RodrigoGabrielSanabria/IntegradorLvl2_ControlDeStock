1ae3ade8f49632f3a33b5c8eea6d7391d35e3a97
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2023-11-14 09:58:17 -0300

    Lectura de la tabla Categorias

---
+++ IntegradorLvl2/Coneccion_DB/CategoriasDB.cs
@@ -0,0 +1,60 @@
+﻿using Dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coneccion_DB
+{
+    public class CategoriasDB
+    {
+
+        public List<Categorias> Listar()
+        {
+
+            List <Categorias> list = new List<Categorias>();
+
+            AccesoDatos datos = new AccesoDatos();
+
+            //Lectura del contenido en la tabla Categorias 
+
+            try
+            {
+
+                datos.SetearConsulta("Select Id, Descripcion From categorias");
+
+                datos.EjecutarLectura();
+
+                while (datos.Lector.Read())
+                {
+                    Categorias aux = new Categorias();
+                    
+                    aux.Id = (int)datos.Lector["Id"];
+
+                    aux.Descripcion = (string)datos.Lector["Description"];
+                
+                    list.Add(aux);
+                }
+
+
+                return list;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+            finally 
+            { 
+                datos.CerrarConexion();
+                 
+            
+            }
+        
+        }
+        
+    }
+}
