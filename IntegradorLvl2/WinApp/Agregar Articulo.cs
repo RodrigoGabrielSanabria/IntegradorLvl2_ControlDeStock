7d8bb408d9975da67bdee36aff30b65919b3d0f8
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2023-11-16 01:36:40 -0300

    Nuevos metodos y configuracion
    configuración del método para cargar los datos de categorías y marcas en los desplegables.
    Configuración del botón aceptar.

--- IntegradorLvl2/WinApp/Agregar Articulo.cs
+++ IntegradorLvl2/WinApp/Agregar Articulo.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dominio;
+using Conexion_DB;
 
 namespace WinApp
 {
@@ -17,9 +19,72 @@ namespace WinApp
             InitializeComponent();
         }
 
+        //Carga la lista de opciones en el desplegable
+        private void Agregar_Articulo_Load(object sender, EventArgs e)
+        {
+
+            CategoriasDB agregarCategoria = new CategoriasDB();
+            MarcasDB agregarMarca = new MarcasDB();
+            try
+            {
+
+                cmbCategoria.DataSource = agregarCategoria.Listar();
+
+                cmbMarca.DataSource = agregarMarca.Listar();
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+
+        }
+
         private void btnCancelar_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+
+            Articulos nuevoArt = new Articulos();
+
+            ArticulosDB articulo = new ArticulosDB(); 
+
+            try
+            {
+                    //Carga los datos ingresados en los texbox            
+                nuevoArt.CodigoArticulo = txbCodigo.Text;
+
+                nuevoArt.Nombre = txbNombre.Text;
+
+                nuevoArt.Descripcion = txbDescripcion.Text; 
+
+                nuevoArt.Precio = double.Parse(txbPrecio.Text);
+
+                nuevoArt.Categorias = (Categorias)cmbCategoria.SelectedItem;
+
+                nuevoArt.Marcas = (Marcas)cmbMarca.SelectedItem;  
+
+
+                //Llama al metodo creado en ArticulosDB 
+
+                articulo.AgregarArticulo(nuevoArt);
+
+                MessageBox.Show("Agregado Exitosamente");
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+
+               MessageBox.Show(ex.ToString());
+            }
+        }
+
+        
     }
 }
