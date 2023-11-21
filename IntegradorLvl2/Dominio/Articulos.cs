d516be9342e0c72a3188fe6cd93cf7a62ac79a46
julio758 <59510875+julio758@users.noreply.github.com>
2023-11-20 22:10:31 -0300

    Pruebas para agregar mas de una palabra a una columna o separaciones

--- IntegradorLvl2/Dominio/Articulos.cs
+++ IntegradorLvl2/Dominio/Articulos.cs
@@ -3,16 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 
 namespace Dominio
 {
     public class Articulos
     {
+        
         public string CodigoArticulo { get; set; }
 
         public string Nombre { get; set; }
-
+        [DisplayName ("Descripción")]//se utiliza para visualizar el acento o para mostrar
+                                     //mas de una palabra o separaciones etc
         public string Descripcion { get; set; }
 
         public Marcas Marcas { get; set; }
