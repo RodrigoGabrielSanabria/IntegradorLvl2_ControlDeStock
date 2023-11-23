cf0841d7f708776f70c0791ee01664abca41f7fe
Rodrigo Gabriel Sanabria <sanabriarodrigo50@gmail.com>
2023-11-23 02:24:16 -0300

    Nuevas Creaciones
    Cambio de la consulta SQL
    Lógica para setear los datos por parámetro en el botón ModificarArticulo

--- IntegradorLvl2/Coneccion_DB/ArticulosDB.cs
+++ IntegradorLvl2/Coneccion_DB/ArticulosDB.cs
@@ -25,7 +25,7 @@ namespace Conexion_DB
 
                 comando.CommandType = System.Data.CommandType.Text;
 
-                comando.CommandText = "SELECT Codigo, Nombre, A.Descripcion, M.Descripcion, C.Descripcion, ImagenUrl,Precio from dbo.ARTICULOS A, dbo.categorias C, dbo.marcas M where M.Id=A.IdMarca and C.Id=A.IdCategoria"; //Insertar consulta
+                comando.CommandText = "SELECT Codigo, Nombre, A.Descripcion, M.Descripcion, C.Descripcion, ImagenUrl,Precio, A.IdMarca, A.IdCategoria, A.Id from dbo.ARTICULOS A, dbo.categorias C, dbo.marcas M where M.Id=A.IdMarca and C.Id=A.IdCategoria\r\n"; //Insertar consulta
                 
                 comando.Connection = conexion;
 
@@ -37,12 +37,15 @@ namespace Conexion_DB
                 while (lector.Read())
                 {
                     Articulos aux = new Articulos();
+                    aux.Id = (int)lector["Id"];
                     aux.CodigoArticulo = lector.GetString(0);
                     aux.Nombre = lector.GetString(1);
                     aux.Descripcion = lector.GetString(2);
                     aux.Marcas = new Marcas();
+                    aux.Marcas.Id = (int)lector["IdMarca"];
                     aux.Marcas.Descripcion = lector.GetString(3);
                     aux.Categorias = new Categorias();
+                    aux.Categorias.Id = (int)lector["IdCategoria"];
                     aux.Categorias.Descripcion = lector.GetString(4);
                     if(!(lector["ImagenURL"] is DBNull)) //Se utiliza para hacer una comprobacion
                     aux.ImagenURL = lector.GetString(5); //Para saber si el valor es nulo
@@ -99,7 +102,38 @@ namespace Conexion_DB
         
         }
 
-        public void ModificarArticulo(Articulos modificar) { }
+        public void ModificarArticulo(Articulos modificar) 
+        {
+            AccesoDatos datos = new AccesoDatos();
+            try
+            {
+
+                datos.SetearConsulta("update ARTICULOS set Codigo = @Codigo, Nombre = @Nombre, Descripcion = @Descripcion, IdMarca = @IdMarca, IdCategoria = @IdCategoria, ImagenUrl= @ImagenUrl, Precio = @Precio where Id = @Id");
+
+                datos.setearParametro("@Codigo", modificar.CodigoArticulo);
+                datos.setearParametro("@Nombre", modificar.Nombre);
+                datos.setearParametro("@Descripcion", modificar.Descripcion);
+                datos.setearParametro("@IdMarca", modificar.Marcas.Id);
+                datos.setearParametro("@IdCategoria", modificar.Categorias.Id);
+                datos.setearParametro("@ImagenUrl", modificar.ImagenURL);
+                datos.setearParametro("@Precio", modificar.Precio);
+                datos.setearParametro("@Id", modificar.Id);
+
+                datos.EjecutarAccion();
+
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            finally
+            { 
+                datos.CerrarConexion();
+            }
+        
+        
+        }
  
     
     }
